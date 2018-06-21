8c2b66e530d5726fb4748bc47c60a569bea73812
Alex Karadimos <karadalex@gmail.com>
2018-06-21 13:58:39 +0300

    simple todos API endpoint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+const port = 5000;
+
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.get('/api/v1/todos', (req, res) => {
+  const todos = [
+    {id:1, task:"Go to supermarket", completed:false},
+    {id:2, task:"Do the laundry", completed:true},
+    {id:3, task:"Study for exams", completed:false}
+  ];
+  res.json(todos);
+})
+
+http.listen(port, () => {
+  console.log(`listening on port:${port}`);
 });
