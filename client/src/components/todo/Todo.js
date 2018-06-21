474e666b66c2ae7cda0692f65f05845795817c97
Alex Karadimos <karadalex@gmail.com>
2018-06-21 14:42:33 +0300

    Separate Todo component

---
+++ client/src/components/todo/Todo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Todo.css';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+class Todo extends Component {
+  render() {
+    return (
+      <Paper className="Todo" elevation={4}>
+        <Typography variant="headline" component="h3">
+          {this.props.name}
+        </Typography>
+        <Typography component="p">
+          {this.props.task}
+        </Typography>
+      </Paper>
+    );
+  }
+}
+
+export default Todo;
