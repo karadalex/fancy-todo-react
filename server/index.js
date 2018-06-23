c854387f637bdf04a04e689b82497e6c113c8956
Alex Karadimos <karadalex@gmail.com>
2018-06-23 16:45:38 +0300

    post a todo

--- server/index.js
+++ server/index.js
@@ -1,19 +1,29 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 
+//configure body-parser for express
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 const port = 5000;
+
+var todos = [
+  {id:1, title: "Supermarket", task:"Go to supermarket", completed:false},
+  {id:2, title: "Laundry", task:"Do the laundry", completed:true},
+  {id:3, title: "Exams", task:"Study for exams", completed:false}
+];
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
 app.get('/api/v1/todos', (req, res) => {
-  const todos = [
-    {id:1, title: "Supermarket", task:"Go to supermarket", completed:false},
-    {id:2, title: "Laundry", task:"Do the laundry", completed:true},
-    {id:3, title: "Exams", task:"Study for exams", completed:false}
-  ];
   res.json(todos);
+})
+
+app.post('/api/v1/todo', (req, res) => {
+  todos.push(req.body);
 })
 
 http.listen(port, () => {
