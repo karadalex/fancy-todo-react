9ba80926b55755811abaadef7675616e9e18a1c8
Alex Karadimos <karadalex@gmail.com>
2018-06-23 13:55:25 +0300

    editable todo

--- client/src/components/todo/Todo.js
+++ client/src/components/todo/Todo.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import './Todo.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+
+const styles = theme => ({
+  title: {
+    fontSize: '2em',
+    paddingBottom: '20px'
+  }
+});
 
 class Todo extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
       <Paper className="Todo" elevation={4}>
-        <Typography variant="headline" component="h3" className="TodoTitle">
-          {this.props.title}
-        </Typography>
-        <Typography component="p">
-          {this.props.task}
-        </Typography>
+        <Input
+          margin="normal"
+          multiline
+          disableUnderline="true"
+          fullWidth="true"
+          className={classes.title}
+          defaultValue={this.props.title}
+        />
+        <Input
+          rowsMax="5"
+          multiline
+          margin="normal"
+          disableUnderline="true"
+          defaultValue={this.props.task}
+        />
       </Paper>
     );
   }
 }
 
-export default Todo;
+Todo.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Todo);
